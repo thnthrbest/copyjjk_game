0a6298634562e03aba3ad57422847265f22d2432
Banana2077 <29656@sa.ac.th>
2026-07-22 08:30:23 +0700

    add exit button in main menu

---
+++ Assets/Scripts/UI/Exitgame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exitgame : MonoBehaviour
+{
+    public void Quit_Game()
+    {
+        Application.Quit();
+    }
+}
